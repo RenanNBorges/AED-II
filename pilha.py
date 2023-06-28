d6a55867760e35361639d3869c1783792ae47c23
Renan Borges <renan.n.borges@hotmail.con>
2023-06-28 18:59:09 -0300

    Add push method in pilha

--- pilha.py
+++ pilha.py
@@ -13,6 +13,7 @@ Restrições de Operações:
  - pop (Excluir o nó no topo da pilha)
  - top (COnsulta o nó no topo da pilha)
  - destroy (Destrói a pilha)
+ - isEmpty (Verifica se a pilha est[a vazia)
 
 --------------------- TIPOS ---------------------
  + CONTIGUIDADE FÍSICA:
@@ -27,3 +28,33 @@ class Nodo:
     def __init__(self,data):
         self.data = data
         self.next = None
+
+class StackConPhy:
+    def __init__(self,size):
+        '''Construtor da Pilha de Contiguidade Fisica
+        Atrbutos:
+            + limite - Limite da pilha
+            + Vetor - Array com os possiveis indices da pilha
+            + Base - Indice que inicia a pilha
+            + Topo - Indice que representa o topo
+        '''
+        self.lim = size - 1
+        self.vector = [None] * size
+        self.base = 0
+        self.top = self.base - 1
+
+    def isEmpty(self):
+        ''' Method to check if stack is empty or not, return a bool'''
+        if self.top < 0:
+            return True
+        else:
+            return False
+
+    def push(self,data):
+        ''' Method to add a new data to top of stack, return a bool about te success of method'''
+        if not(self.isEmpty()):
+            self.top = self.top + 1
+            self.vector[self.top] = data
+            return True
+        else:
+            return False
