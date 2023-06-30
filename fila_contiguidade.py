8b229c3b5e72ba4150a031a76c4ae9a85d9f5285
Renan N. Borges <renan.n.borges@hotmail.com>
2023-06-30 20:15:15 -0300

    Add file lista_contiguidade.py

---
+++ fila_contiguidade.py
@@ -0,0 +1,25 @@
+"""
+Feito por Renan Nunes Borges
+
+Arquivo contendo representações de Tipos Abstratos de Dados Fila
+
+Filas usam do príncipio de FIFO (First in, First Out)
+Tem acesso restrito há somente o primeiro elemento que foi inserido
+OPERAÇÕES que são afetas pelas restrições:
+ * Inserir
+ * Excluir
+ * Consultar
+
+--------------------- OPERAÇÕES ---------------------
+• criar uma fila vazia
+• inserir um nó no final da fila
+• excluir o nó do início da fila
+• consultar nó do início da fila
+• destruir a fila
+
+Filas de contiguidade física tem as seguintes propriedades:
+LI : limite inferior da área
+ini : início da fila
+fim : final da fila
+LS : limite superior da área
+"""
