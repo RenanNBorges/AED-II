3b5da7aa2a58c5e474af55146c735350766568cc
Renan N. Borges <renan.n.borges@hotmail.com>
2023-07-04 14:11:56 -0300

    Added file fila_encadeada.py

---
+++ fila_encadeada.py
@@ -0,0 +1,23 @@
+"""
+Feito por Renan Nunes Borges
+
+Arquivo contendo representações de Tipos Abstratos de Dados Fila
+
+Filas usam do príncipio de FIFO (First in, First Out)
+Tem acesso restrito há o primeiro e ultimo elemento que foi inserido
+Mas as operações de consulta e exclusão são somente pelo primeiro
+OPERAÇÕES que são afetas pelas restrições:
+ * Inserir
+ * Excluir
+ * Consultar
+
+--------------------- OPERAÇÕES ---------------------
+• criar uma fila vazia
+• inserir um nó no final da fila
+• excluir o nó do início da fila
+• consultar nó do início da fila
+• destruir a fila
+
+Filas encadeadas tem as seguintes propriedades:
+Um descritor com o n[o de inicio e fim
+"""
