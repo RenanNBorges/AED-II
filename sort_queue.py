e9c8c4d8ef64044be79a18681a2018195e061524
Renan N. Borges <renan.n.borges@hotmail.com>
2023-07-05 10:51:04 -0300

    Fix function sorting_queue and create file tabela_ex.py

--- sort_queue.py
+++ sort_queue.py
@@ -8,7 +8,7 @@ def sorting_queue(queue_unorganized: DynamicQueue):
     s_assistant = DynamicStack()
 
     while not(queue_unorganized.is_empty()):
-        if (s_sorted.is_empty() and s_assistant.is_empty()) or queue_unorganized.peek() < s_sorted.peek():
+        if (s_sorted.is_empty()) or queue_unorganized.peek() <= s_sorted.peek():
             s_sorted.push(queue_unorganized.peek())
             queue_unorganized.remove()
             while not(s_assistant.is_empty()):
@@ -33,6 +33,11 @@ queue.insert(1)
 queue.insert(8)
 queue.insert(3)
 queue.insert(5)
+queue.insert(10)
+queue.insert(1)
+queue.insert(8)
+queue.insert(3)
+queue.insert(50)
 print(queue)
 print(sorting_queue(queue))
 
---
+++ tabela_ex.py
@@ -0,0 +1,16 @@
+from tabela import Table
+
+
+test_table = Table(10)
+
+test_table.insert("ABC1234", ("VW", "GOL", 2010, "Anderson"))
+test_table.insert("DFG6789", ('FIAT', 'UNO', '2001', 'Maria'))
+test_table.insert("JKL0077 ", ('FIAT', 'UNO', '2001', 'Maria'))
+test_table.insert("UTY2345 ", ('FIAT', 'UNO', '2001', 'Maria'))
+test_table.insert("XYZ4567 ", ('FIAT', 'UNO', '2001', 'Maria'))
+print(test_table)
+test_table.insert("XYZ4567 ", ('FORD', 'FOCUS', '2007', 'Carlos'))
+print(test_table)
+
+test_table.remove("DFG6789")
+print(test_table)
