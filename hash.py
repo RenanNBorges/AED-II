3fd233bd1467d9c0af1940205531a8d348c1b20c
Renan N. Borges <renan.n.borges@hotmail.com>
2023-07-07 23:33:59 -0300

    tabela_hash of registro

--- hash.py
+++ hash.py
@@ -1,10 +1,13 @@
 
 from tabela import Table
+
+
 def hash(key,size):
     return key % (size + 1)
 
 
-registro = Table(8)
+registro = Table(7)
+
 registro.insert(73, 'João')
 registro.insert(44, 'Carlos')
 registro.insert(37, 'Carlos')
@@ -13,3 +16,9 @@ registro.insert(59, 'Ronaldo')
 registro.insert(61, 'Michel')
 registro.insert(99, 'Carlos')
 
+keys = []
+tabela_hash = Table(7)
+for i in range(1, 8):
+    hash_index = hash(registro.key[i],7)
+    tabela_hash.insert(hash_index,registro.key[i])
+    print(hash_index)
