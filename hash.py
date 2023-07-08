acf63c25cb450576f7755e41821c1ef6ead52776
Renan N. Borges <renan.n.borges@hotmail.com>
2023-07-07 23:43:57 -0300

    Annotations added

--- hash.py
+++ hash.py
@@ -1,8 +1,7 @@
-
 from tabela import Table
 
 
-def hash(key,size):
+def hash(key, size):
     return key % (size + 1)
 
 
@@ -19,6 +18,8 @@ registro.insert(99, 'Carlos')
 keys = []
 tabela_hash = Table(7)
 for i in range(1, 8):
-    hash_index = hash(registro.key[i],7)
-    tabela_hash.insert(hash_index,registro.key[i])
+    hash_index = hash(registro.key[i], 7)
+    tabela_hash.insert(hash_index, registro.key[i])
     print(hash_index)
+
+# TODO Add Open addressing Linked
